a3a23fa7da3c96055a3888d3e2fcce29334bc752
Fallen_Breath <nellaforax@outlook.com>
2025-11-17 00:30:18 +0800

    1.16.5 works

--- src/main/java/com/example/GuiConfigs.java
+++ src/main/java/com/example/GuiConfigs.java
@@ -1,7 +1,20 @@
 package com.example;
 
-import fi.dy.masa.malilib.gui.GuiBase;
+import fi.dy.masa.malilib.gui.GuiConfigsBase;
+import net.minecraft.client.gui.screens.Screen;
 
-public class GuiConfigs extends GuiBase
+import java.util.List;
+
+public class GuiConfigs extends GuiConfigsBase
 {
+	public GuiConfigs(int listX, int listY, String modId, Screen parent, String titleKey, Object... args)
+	{
+		super(listX, listY, modId, parent, titleKey, args);
+	}
+
+	@Override
+	public List<ConfigOptionWrapper> getConfigs()
+	{
+		return List.of();
+	}
 }
