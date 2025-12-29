e29dfd01d352a968b0e48b38ae963ef3c37b2a6e
Fallen_Breath <nellaforax@outlook.com>
2025-12-29 20:30:03 +0800

    case: useLegacyMixinAp false, without modCompileOnly -- error

--- src/main/java/com/example2/mixin/ExampleMixin2.java
+++ src/main/java/com/example2/mixin/ExampleMixin2.java
@@ -10,11 +10,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Pseudo
 @Mixin(targets = "com.example.ExampleHelper")
 public class ExampleMixin2 {
-	@Inject(at = @At("HEAD"), method = "myBlockPosBelow1(Lcom/example/ExampleHelper;Lnet/minecraft/core/BlockPos;)Lnet/minecraft/core/BlockPos;")
-	private static void init1(CallbackInfoReturnable<BlockPos> cir) {
-	}
-
-	@Inject(at = @At("HEAD"), method = "myBlockPosBelow2(Lnet/minecraft/core/BlockPos;)Lnet/minecraft/core/BlockPos;")
-	private static void init2(CallbackInfoReturnable<BlockPos> cir) {
+	@Inject(at = @At("HEAD"), method = "myBlockPosBelow(Lnet/minecraft/core/BlockPos;)Lnet/minecraft/core/BlockPos;")
+	private static void init(CallbackInfoReturnable<BlockPos> cir) {
 	}
 }
